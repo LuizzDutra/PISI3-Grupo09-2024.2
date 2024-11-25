5a79cb4845f652e491b975f86eeeb1069c84d047
LuizzDutra <luizzidutra@gmail.com>
2024-11-25 18:04:34 -0300

    Desenvolvimento das descrições

--- main.py
+++ main.py
@@ -78,7 +78,12 @@ def mostrar_analise_prazos():
                     labels={'on_time': 'Entrega no Prazo', 'order_count': 'Número de Pedidos'},
                     title='Número de Pedidos Entregues no Prazo vs. Fora do Prazo')
         fig1.update_xaxes(tickvals=[0, 1], ticktext=['Fora do Prazo', 'No Prazo'])
+    st.markdown('''>Agrupamos os dados da coluna on_time e usamos o count para adquirir a contagem de pedidos fora e dentro do prazo.''')
     st.plotly_chart(fig1)
+    st.markdown('''
+                >Observa-se que a maioria dos pedidos são entregues no prazo.
+                >Mas ainda cerca de 10% chegam atrasados.
+                ''')
 
     with st.echo():
         # Merge the dataframes
@@ -91,7 +96,12 @@ def mostrar_analise_prazos():
                     labels={'customer_state': 'Estado do Cliente', 'order_count': 'Número de Pedidos', 'on_time': 'Entrega no Prazo'},
                     title='Número de Pedidos Entregues no Prazo vs. Fora do Prazo por Estado',
                     color_continuous_scale=["red", "blue"])
+    st.markdown('''
+                >Primeiramente é feito um merge entre pedidos e clientes para se ter acesso a coluna customer_state.
+                Após isso agrupamos com a coluna customer_state e on_time e é extraída a contagem''')
     st.plotly_chart(fig2)
+    st.markdown('''
+                >Com este gráfico tem o intuito de tentar visualizar se algum estado em particular possui algum problema com prazos de entrega, entretanto, os estados parecem ter uma proporção similar entre eles.''')
 
 
     with st.echo():
@@ -104,7 +114,16 @@ def mostrar_analise_prazos():
                     labels={'product_category_name': 'Categoria do Produto', 'order_count': 'Número de Pedidos', 'on_time': 'Entrega no Prazo'},
                     title='Atraso na Entrega por Categoria de Produto',
                     color_continuous_scale=["red", "blue"])
+    st.markdown('''
+                >Aqui é reutilizada a "merged_df" do código anterior além de ser feito um merge com df_itens e df_produtos.
+                Assim sendo possível relacionar a coluna on_time com a categoria do produto através do groupby.
+                Após isso é feita a contagem de produtos por categoria, classificados entre dentro e fora do prazo.''')
     st.plotly_chart(fig3)
+    st.markdown('''
+                >Este gráfico possue as entregas separadas por categoria e uma divisão com cor referente ao prazo.
+                O motivo é a observação de uma possível categoria problemática nas entregas, mas não há uma categoria de produto
+                com problemas de atraso aparentes.
+                ''')
 
 
 
@@ -113,16 +132,21 @@ def mostrar_trend_compras():
         merged_df = pd.merge(df_itens, df_produtos, on='product_id', how='left')
         merged_df = pd.merge(merged_df, df_pedidos, on='order_id', how='left')
 
-        # 2. Price Category (can be customized):
-        price_bins = [0, 50, 100, 200, float('inf')]
-        price_labels = ['0-50', '51-100', '101-200', '200+']
-        merged_df['price_category'] = pd.cut(merged_df['price'], bins=price_bins, labels=price_labels, right=False)
-
-        #1. Product Category Popularity:
         product_popularity = merged_df.groupby('product_category_name')['order_id'].count().reset_index()
         product_popularity = product_popularity.rename(columns={'order_id': 'order_count'})
         fig1 = px.bar(product_popularity, x='order_count', y='product_category_name', title='Popularidade das categorias de produto')
+    st.markdown('''
+                >Primeiramente é feito o merge entre itens produtos e pedidos, para serem utilizadas as colunas de
+                categoria de item e preço que seram relevantes no uso do merged_df.
+                Após isso é realizado um groupby na categoria e feito uma agregação de count, para alcançar a quantidade de compras
+                de determinada categoria.
+                ''')
     st.plotly_chart(fig1)
+    st.markdown('''
+                >Com esse gráfico se obtem como categorias de produto mais compradas sendo:
+                
+                >cama_mesa_banho, beleza_saude e esporte_lazer.
+                ''')
 
     
 
@@ -130,21 +154,46 @@ def mostrar_trend_compras():
     with st.echo():
         average_price_by_category = merged_df.groupby('product_category_name')['price'].mean().reset_index()
         fig2 = px.bar(average_price_by_category, x='price', y='product_category_name', title='Preço médio por categoria de produto')
+    st.markdown('''
+                >Neste código é realizado o cálculo das médias de preço por categoria de produto.
+                ''')
     st.plotly_chart(fig2)
+    st.markdown('''
+                >Com esse gráfico é possível observar que a categoria com valor de compra mais elevado é a de computadores(pcs).
+                
+                >Mas é necessário se atentar que é uma das categorias menos compradas, com 203 compras no dataset.
+                ''')
 
     with st.echo():
         merged_df['purchase_month'] = merged_df['order_purchase_timestamp'].dt.month
         monthly_purchases = merged_df.groupby('purchase_month')['order_id'].count().reset_index()
         monthly_purchases = monthly_purchases.rename(columns={'order_id':'order_count'})
         fig3 = px.line(monthly_purchases, x='purchase_month', y='order_count', title='Quantidade de compras por mês')
+    st.markdown('''
+                >Aqui é criada uma coluna no merged_df denominada de purchase_month com o intuito de realizar a contagem de compras
+                por mês.
+                ''')
     st.plotly_chart(fig3)
+    st.markdown('''
+                >É possível observar que entre os mêses 3 e 8 se tem maiores quantidades de pedidos no dataset.
+                ''')
 
 
     with st.echo():
+        price_bins = [0, 50, 100, 200, float('inf')]
+        price_labels = ['0-50', '51-100', '101-200', '200+']
+        merged_df['price_category'] = pd.cut(merged_df['price'], bins=price_bins, labels=price_labels, right=False)
         price_category_purchases = merged_df.groupby('price_category', observed=True)['order_id'].count().reset_index()
         price_category_purchases = price_category_purchases.rename(columns={'order_id':'order_count'})
         fig4 = px.bar(price_category_purchases, x='price_category', y='order_count', title='Valor de compra por categoria')
+    st.markdown('''
+                >Neste código é realizada a separação de compras em categorias de preço através do cut.
+                Após isso é feita a contagem das compras através do agrupamento das categorias.
+                ''')
     st.plotly_chart(fig4)
+    st.markdown('''
+                No gráfico é possível observar uma relação inversamente proporcional esperada entre valor de compra e número de pedidos.
+                ''')
 
 def analise_avaliacoes():
     with st.echo():
